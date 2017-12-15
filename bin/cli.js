2460808eded61d696d301c5e4872c50a257e6b9e
remyyounes <ryounes@gmail.com>
2017-12-15 10:44:08 -0800

    filters and sorts

--- bin/cli.js
+++ bin/cli.js
@@ -1,32 +1,21 @@
 #!/usr/bin/env node
-var foo = require('../index.js')
-const {
-  debug,
-  fullChangelog,
-  lernaChangelog,
-  recentChangelog
-} = foo
+var lib = require("../index.js");
+const { debug, fullChangelog, lernaChangelog, recentChangelog } = lib;
 
-var flags = require('yargs')
-  .version(require('../package').version)
-  .usage('\nUsage: $0 [options]')
-  .option('all', {
-    description: 'start tag',
-  })
-  .option('from', {
-    description: 'start tag',
-  })
-  .option('to', {
-    description: 'end tag',
-  })
-  .help('help')
-  .showHelpOnFail(true, 'use --help for available options').argv
-
+var flags = require("yargs")
+  .version(require("../package").version)
+  .usage("\nUsage: $0 [options]")
+  .option("all", { description: "start tag" })
+  .option("preid", { description: "lerna publish preid" })
+  .option("from", { description: "start tag" })
+  .option("to", { description: "end tag" })
+  .help("help")
+  .showHelpOnFail(true, "use --help for available options").argv;
 
 if (flags.all) {
-  fullChangelog().then(debug)
+  fullChangelog().then(debug);
 } else if ((flags.from && flags.to) || flags.from) {
-  lernaChangelog(flags.from, flags.to).then(debug)
+  lernaChangelog({ version: flags.from }, { version: flags.to }).then(debug);
 } else {
-  recentChangelog().then(debug)
+  recentChangelog().then(debug);
 }
--- index.js
+++ index.js
@@ -1,47 +1,101 @@
-const { exec } = require('child_process')
+const { exec } = require("child_process");
+const { split, head, last, join, compose, map, filter } = require("ramda");
 
-const toArray = str => str.split('\n')
+const TAG_SPLIT = "\n## ";
+
+const toArray = str => str.split("\n");
+const prepend = pre => str => pre.concat(str);
+
 const debug = x => {
-  console.log(x)
+  console.log(x);
+  debugger;
   return x;
-}
-const removeBlanks = arr => arr.filter(str => str)
+};
 
-const tagFrom = from => (from ? `--tag-from ${from}` : '')
+const removeBlanks = arr => arr.filter(str => str);
 
-const tagTo = to => (to ? `--tag-to ${to}` : '')
+const isUnreleased = tag => tag.indexOf("Unreleased") === 0;
+
+const tagFrom = from => (from ? `--tag-from ${from}` : "");
+
+const tagTo = to => (to ? `--tag-to ${to}` : "");
+
+const parseTagVersion = tag =>
+  tag
+    .split(" ")[0]
+    .split("-")[0]
+    .trim();
+
+const getPrevStableTag = tags => {
+  return tags.filter(tag => !tag.includes(tags[0]))[0];
+};
+
+const filterTags = tags => {
+  let version = null;
+  return tags.filter(body => {
+    const curVersion = parseTagVersion(body);
+    if (isUnreleased(body) || curVersion.includes(version)) {
+      return false;
+    }
+    version = curVersion;
+    return true;
+  });
+};
+
+const sortTags = versions => tags =>
+  tags.sort((a, b) => {
+    const dateA = versions[a] && versions[a].date;
+    const dateB = versions[b] && versions[b].date;
+    return dateA - dateB;
+  });
+
+const squashVersions = versions =>
+  compose(
+    prepend(TAG_SPLIT),
+    join(TAG_SPLIT),
+    sortTags(versions),
+    filterTags,
+    removeBlanks,
+    split(TAG_SPLIT)
+  );
+
+const toTag = meta => {
+  const [version, date] = meta.split("|");
+  return { version, date: parseInt(date, 10) };
+};
 
 const getTags = () =>
   new Promise((resolve, reject) =>
     exec(
-      "git for-each-ref --sort=-taggerdate --format '%(tag)' refs/tags",
+      "git for-each-ref --sort=-taggerdate --format '%(tag)|%(taggerdate:raw)' refs/tags",
       (err, stdout, stderr) => (err ? reject(err) : resolve(stdout))
     )
   )
     .then(toArray)
     .then(removeBlanks)
+    .then(map(toTag));
 
-const lernaChangelog = (from, to) =>
+const lernaChangelog = (from = {}, to = {}) =>
   new Promise((resolve, reject) =>
     exec(
-      debug(`npx lerna-changelog ${tagFrom(from)} ${tagTo(to)}`),
+      `npx lerna-changelog ${tagFrom(from.version)} ${tagTo(to.version)}`,
       (err, stdout, stderr) => (err ? reject(err) : resolve(stdout))
     )
-  )
+  );
 
-const rangeChangelog = lernaChangelog
+const fullChangelog = () =>
+  getTags().then(tags => lernaChangelog(last(tags)).then(squashVersions(tags)));
 
-const fullChangelog = () => getTags()
-    .then(tags => lernaChangelog(tags.pop()))
-
-
-const recentChangelog = () => getTags()
-    .then(tags => lernaChangelog(tags[1], tags[0]))
-
+const recentChangelog = () =>
+  getTags().then(tags =>
+    lernaChangelog(getPrevStableTag(tags), head(tags)).then(
+      squashVersions(tags)
+    )
+  );
 
 module.exports = {
   debug,
   fullChangelog,
-  lernaChangelog
-  recentChangelog,
-}
+  lernaChangelog,
+  recentChangelog
+};
