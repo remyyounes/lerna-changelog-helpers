3f16ae4fd76f66a0ed60fa0ffc016485a68c21f0
remyyounes <ryounes@gmail.com>
2017-12-15 20:25:04 -0800

    use ramda

--- index.js
+++ index.js
@@ -1,70 +1,69 @@
-const { exec } = require('child_process')
-const R = require('ramda')
+const { exec } = require("child_process");
+const R = require("ramda");
 
-const TAG_SPLIT = '\n## '
+const TAG_SPLIT = "\n## ";
 
 const debug = x => {
-  console.log(x)
-  debugger
-  return x
-}
+  console.log(x);
+  debugger;
+  return x;
+};
 
-const prepend = pre => str => `${pre}${str}`
+const prepend = pre => str => `${pre}${str}`;
 
-const removeBlanks = R.filter(R.identity)
+const removeBlanks = R.filter(R.identity);
 
-const isReleased = R.pipe(R.startsWith('Unreleased'), R.not)
+const isReleased = R.pipe(R.startsWith("Unreleased"), R.not);
 
-const tagFrom = tag => (tag ? `--tag-from ${tag.version}` : '')
+const tagFrom = tag => (tag ? `--tag-from ${tag.version}` : "");
 
-const tagTo = tag => (tag ? `--tag-to ${tag.version}` : '')
+const tagTo = tag => (tag ? `--tag-to ${tag.version}` : "");
 
 const parseTagVersion = R.pipe(
-  R.split(' '),
+  R.split(" "),
   R.head,
-  R.split('-'),
+  R.split("-"),
   R.head,
   R.trim
-)
+);
 
-const buildTimestampHash = tags =>
-  tags.reduce((acc, tag) => {
-    acc[tag.version] = tag.date
-    return acc
-  }, {})
+const buildTimestampHash = R.reduce((acc, tag) => {
+  acc[tag.version] = tag.date;
+  return acc;
+}, {});
 
-const getPrevStableTag = (latest, tags) =>
-  R.head(tags.filter(tag => !tag.version.includes(latest)))
+const getPrevStableTag = latest =>
+  R.compose(R.head, R.filter(tag => !tag.version.includes(latest)));
 
 const getLatestTag = R.pipe(
   R.split(TAG_SPLIT),
   removeBlanks,
   R.head,
   prepend(TAG_SPLIT)
-)
+);
 
 const removePrereleases = tags => {
-  const versions = {}
+  const versions = {};
 
   return tags.reduce((acc, tag) => {
-    const version = parseTagVersion(tag)
+    const version = parseTagVersion(tag);
 
     if (!versions[version]) {
-      versions[version] = true
-      acc.push(tag)
+      versions[version] = true;
+      acc.push(tag);
     }
 
-    return acc
-  }, [])
-}
+    return acc;
+  }, []);
+};
 
 const sortTags = timestampHash => mdTags =>
   mdTags.sort((a, b) => {
-    const versionA = parseTagVersion(a)
-    const versionB = parseTagVersion(b)
+    const versionA = parseTagVersion(a);
+    const versionB = parseTagVersion(b);
 
-    return timestampHash[versionB] - timestampHash[versionA]
-  })
+    return timestampHash[versionB] - timestampHash[versionA];
+  });
 
 const squashVersions = tags =>
   R.pipe(
@@ -75,12 +74,12 @@ const squashVersions = tags =>
     sortTags(buildTimestampHash(tags)),
     R.join(TAG_SPLIT),
     prepend(TAG_SPLIT)
-  )
+  );
 
 const toTag = meta => {
-  const [version, date] = meta.split('|')
-  return { version, date: parseInt(date, 10) }
-}
+  const [version, date] = meta.split("|");
+  return { version, date: parseInt(date, 10) };
+};
 
 const getTags = () =>
   new Promise((resolve, reject) =>
@@ -88,14 +87,14 @@ const getTags = () =>
       "git for-each-ref --sort=-taggerdate --format '%(tag)|%(taggerdate:raw)' refs/tags",
       (err, stdout, stderr) => (err ? reject(err) : resolve(stdout))
     )
-  ).then(parseTags)
+  ).then(parseTags);
 
 const parseTags = R.pipe(
-  R.split('\n'),
+  R.split("\n"),
   R.map(R.trim),
   removeBlanks,
   R.map(toTag)
-)
+);
 
 const lernaChangelog = (from, to) =>
   new Promise((resolve, reject) =>
@@ -103,12 +102,12 @@ const lernaChangelog = (from, to) =>
       `npx lerna-changelog ${tagFrom(from)} ${tagTo(to)}`,
       (err, stdout, stderr) => (err ? reject(err) : resolve(stdout))
     )
-  )
+  );
 
 const fullChangelog = () =>
   getTags().then(tags =>
     lernaChangelog(R.last(tags)).then(squashVersions(tags))
-  )
+  );
 
 const recentChangelog = () =>
   getTags()
@@ -118,7 +117,7 @@ const recentChangelog = () =>
         R.head(tags)
       ).then(getLatestTag)
     )
-    .catch(console.error)
+    .catch(console.error);
 
 module.exports = {
   buildTimestampHash,
@@ -139,5 +138,5 @@ module.exports = {
   squashVersions,
   tagFrom,
   tagTo,
-  toTag,
-}
+  toTag
+};
